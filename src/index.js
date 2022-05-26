75e5137d3e6255e7c77fba49c1040f898ca14a1e
Gifford R Nowland <gifford.r.nowland@aero.org>
2022-05-26 00:23:35 -0700

    remove existing rules

--- src/index.js
+++ src/index.js
@@ -7,20 +7,13 @@
 
 'use strict';
 
-import RulesOfHooks from './RulesOfHooks';
-import ExhaustiveDeps from './ExhaustiveDeps';
 
 export const configs = {
   recommended: {
-    plugins: ['react-hooks'],
     rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 };
 
 export const rules = {
-  'rules-of-hooks': RulesOfHooks,
-  'exhaustive-deps': ExhaustiveDeps,
 };
